14be9c896d3f1cd5a660ae7d085adfdfb0b21b71
Vap0r1ze <superdash993@gmail.com>
2023-12-03 00:00:18 -0500

    more day 2

---
+++ 02/solve.jl
@@ -0,0 +1,34 @@
+using BenchmarkTools
+re = r"(\d+) (.)"
+
+function game_value_sum(map_value)
+    io = open("input_large.txt", "r")
+    sum = 0
+    game = 0
+    for line in eachline(io)
+        game += 1
+
+        draws = map(x -> begin
+                return (parse(Int64, x.captures[1]), x.captures[2])
+            end, collect(eachmatch(re, line)))
+
+        maxes = Dict("r" => 0, "g" => 0, "b" => 0)
+        for (n, color) in draws
+            maxes[color] = max(maxes[color], n)
+        end
+
+        sum += map_value(maxes, game)
+    end
+
+    return sum
+end
+
+function part1()
+    return game_value_sum((m, game) -> begin
+        return game * (m["r"] <= 12 && m["g"] <= 13 && m["b"] <= 14)
+    end)
+end
+
+@info part1()
+# @btime reduce(+, get_games(m -> m["r"] <= 12 && m["g"] <= 13 && m["b"] <= 14))
+# @btime reduce(+, [1, 2, 3])
